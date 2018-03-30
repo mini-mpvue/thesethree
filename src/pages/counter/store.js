08e586c4269c68f6b2401a9aba544092e528f50c
shenqingqing@maxrocky.com <shenqingqing@maxrocky.com>
2018-03-30 17:24:47 +0800

    vuex持久化示例

--- src/pages/counter/store.js
+++ src/pages/counter/store.js
@@ -2,6 +2,8 @@
 // make sure to call Vue.use(Vuex) if using a module system
 import Vue from 'vue'
 import Vuex from 'vuex'
+import createPersistedState from 'vuex-persistedstate'
+import Cookies from 'js-cookie';
 
 Vue.use(Vuex)
 
@@ -59,7 +61,14 @@ const store = new Vuex.Store({
   modules : {
     a:moduleA,
     b:moduleB
-  }
+  },
+  plugins: [createPersistedState({
+    storage: {
+      getItem: key => Cookies.get(key),
+      setItem: (key, value) => Cookies.set(key, value, { expires: 3, secure: true }),
+      removeItem: key => Cookies.remove(key)
+    }
+  })]
  
 })
 
